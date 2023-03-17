b61fba6671a08fc85a2c6b05c509ab20ff6e0a3c
Sam-366 <ntlombe.s@gmail.com>
2023-03-16 17:34:14 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  *  Return: ends the execution of a function
  */
 
-int (main)void
-{
-	puts("\"Programming is like building a multilingual puzzle);
+int (main); void
+'{'
+	puts'('"\"Programming is like building a multilingual puzzle);
 	return (0);
 }
